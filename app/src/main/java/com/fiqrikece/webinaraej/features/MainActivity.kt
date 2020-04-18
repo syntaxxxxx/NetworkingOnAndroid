9d7eb54aa055a6f7ef7b60e46b19f3eaf213902d
syntaxxxxx <fiqrihafzainislami@gmail.com>
2020-04-18 19:37:45 +0700

    starter project

---
+++ app/src/main/java/com/fiqrikece/webinaraej/features/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.fiqrikece.webinaraej.features
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fiqrikece.webinaraej.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/fiqrikece/webinaraej/network/Routes.kt
@@ -0,0 +1,4 @@
+package com.fiqrikece.webinaraej.network
+
+interface Routes {
+}
